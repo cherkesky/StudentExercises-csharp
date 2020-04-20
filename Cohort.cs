e87f2c79e1b72e2fc6d845164c320176620a4974
Cherkesky <guy@cherkesky.com>
2020-04-20 15:46:49 -0500

    moving forward with Cohort clas. corrected Student to support the Cohort type

---
+++ Cohort.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace StudentExcercises{
+class Cohort {
+public Cohort(){
+
+}
+
+
+}
+}
--- Student.cs
+++ Student.cs
@@ -1,17 +1,28 @@
 using System;
 using System.Collections.Generic;
+// Student
+// You must define a type for representing a student in code. A student can only be in one cohort at a time. A student can be working on many exercises at a time.
+// Properties
+// First name
+// Last name
+// Slack handle
+// The student's cohort
+// The collection of exercises that the student is currently working on <------------
 
+namespace StudentExcercises{
 class Student {
-  public Student(string first, string last, string slack, byte cohort){
-    _first = first;
-    _last = last;
-    _slack = slack;
-    _cohort = cohort;
+  public Student(string first, string last, string slack, Cohort cohort){
+   First = first;
+    Last = last;
+    Slack = slack;
+    Cohort = cohort;
   }
-  private string _first;
-  private string _last;
-  private string _slack;
-  private byte _cohort;
+  public string First{get; set;}
+  public string Last{get; set;}
+  public string Slack{get; set;}
+  public Cohort Cohort{get; set;}
 
+  
 
+}
 }
