7d77a79c3295c49ca2464a3bdaa09b887b6afeb2
Cherkesky <guy@cherkesky.com>
2020-04-20 14:38:06 -0500

    initial file structure

---
+++ Student.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+class Student {
+  public Student(string first, string last, string slack, byte cohort){
+    _first = first;
+    _last = last;
+    _slack = slack;
+    _cohort = cohort;
+  }
+  private string _first;
+  private string _last;
+  private string _slack;
+  private byte _cohort;
+
+
+}
